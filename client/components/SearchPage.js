595dd5ad92788c76710f7d38aea6e3050f0d5636
Gary Iacobucci <1161598+garyiacobucci@users.noreply.github.com>
2021-08-25 11:12:06 -0700

    Continued iterating on SearchBar and SearchPage components

--- client/components/SearchPage.js
+++ client/components/SearchPage.js
@@ -1,24 +1,51 @@
-import React from 'react';
+import React from 'react'; // Unnecessary as of React 17: https://reactjs.org/blog/2020/09/22/introducing-the-new-jsx-transform.html
 // import { connect } from 'react-redux';
 import NavBar from './NavBar';
+import SearchBar from './SearchBar';
 
 
-const Search = () => {
+const SearchPage = () => {
 
-  // Build  search bar for user to enter sitewide search
+  // Dummy data for testing:
+  const posts = [
+    { id: '1', name: 'This first post is about React' },
+    { id: '2', name: 'This next post is about Preact' },
+    { id: '3', name: 'We have yet another React post!' },
+    { id: '4', name: 'This is the fourth and final post' },
+  ];
+
+  // We’ll also need a function that filters out posts depending on the search query:
+  const filterPosts = (posts, query) => {
+    if (!query) {
+        return posts;
+    }
   
+    return posts.filter((post) => {
+        const postName = post.name.toLowerCase();
+        return postName.includes(query);
+    });
+  };
 
-  // Query the database for search results
-
-  // Populate a list of results from search
-  let results = 'Hello world!'
+  // Our search bar will navigate us to a new URL when we perform a search.
+  // We can grab this value from the URL:
+  const { search } = window.location;
+  const query = new URLSearchParams(search).get('s');
+  // Using our search query and filter function, we can render the posts that match our search:
+  const filteredPosts = filterPosts(posts, query);
 
     return(
       <>
         <NavBar/>
-        <div>{results}</div>
+        {/*<div>
+            <SearchBar />
+            <ul>
+                {filteredPosts.map((post) => (
+                    <li key={post.id}>{post.name}</li>
+                ))}
+            </ul>
+                </div>*/}
       </>
     )
 }
 
-export default Search;
+export default SearchPage;
