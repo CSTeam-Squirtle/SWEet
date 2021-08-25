355fdad32cf99eda4a49ab262277ff18e848f55d
Ricardo Cortez <ricardo@Ricardos-MacBook-Pro.local>
2021-08-24 19:27:49 -0700

    redux debugged

---
+++ client/components/Main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// import { connect } from 'react-redux';
+// import DailyTask from './DailyTask'
+// import JobApps from './JobApps'
+
+
+const Main = () => {
+    return(
+        <h1>Test</h1>
+    )
+}
+
+export default Main;
