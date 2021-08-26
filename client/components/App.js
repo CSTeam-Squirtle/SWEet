f9b1c091b285ed219e71a9557970f75f7f377d16
Gary Iacobucci <1161598+garyiacobucci@users.noreply.github.com>
2021-08-25 19:53:31 -0700

    Added style-loader and css-loader to Webpack, modified stylesheet, imported stylesheet into App.js, and added relevant dev dependencies to package.json

--- client/components/App.js
+++ client/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Main from './Main';
 import Login from './login'
+import './../css/styles.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
